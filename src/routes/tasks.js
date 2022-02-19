9cff0fc779f407954954eaed4adf29825b76f97c
mehdi-elke <m.elketroussi@gmail.com>
2022-02-19 17:28:36 +0100

    initial commit

---
+++ src/routes/tasks.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var router = express.Router();
+const utils = require('../utils/task-schema.js');
+
+const tasks = [
+    {
+        id: 1,
+        name: "Task 1",
+        completed: false
+    },
+    {
+        id: 2,
+        name: "Task 2",
+        completed: false
+    },
+    {
+        id: 3,
+        name: "Task 3",
+        completed: false
+    }
+];
+
+router.get("/tasks" , (request, response) => {
+    response.send(tasks);
+});
+
+router.get("/tasks/:id" , (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+    response.send(task);
+});
+
+router.post("/tasks", (request, response) => {
+    const { error } = utils.validateTask(request.body);
+
+    if(error) return response.status(400).send("The name should be at least 3 chars long!")
+
+    const task = {
+        id: tasks.length + 1,
+        name: request.body.name,
+        completed: request.body.completed
+    };
+
+    tasks.push(task);
+    response.status(201).send(task);
+});
+
+
+router.put("/tasks/:id", (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+
+    const { error } = utils.validateTask(request.body);
+
+    if(error) return response.status(400).send("The name should be at least 3 chars long!")
+
+    task.name = request.body.name;
+    task.completed = request.body.completed;
+
+    response.send(task);
+});
+
+
+router.patch("/tasks/:id", (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+
+    const { error } = utils.validateTask(request.body);
+
+    if(error) return response.status(400).send("The name should be at least 3 chars long!")
+
+    task.name = request.body.name;
+
+    if(request.body.completed) {
+        task.completed = request.body.completed;
+    }
+    response.send(task);
+});
+
+
+router.delete("/tasks/:id", (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+    response.send(task);
+});
+
+
+module.exports = router;
