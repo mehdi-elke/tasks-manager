3cbc01b9422d4b11c246c16a8c02e272ced15fde
mehdi-elke <m.elketroussi@gmail.com>
2022-02-20 13:46:16 +0100

    add failing test

--- src/routes/users-controller.js
+++ src/routes/users-controller.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var usersRouter = express.Router();
 
-const UserAdapter = require("../models/users/user-adapter.js");
 const GoogleUser = require("../models/users/google-user.js");
 const User = require('../models/users/user.js');
 
@@ -10,9 +9,9 @@ const users = [];
 users.push(new User("Jean"));
 users.push(new User("Lucie"));
 users.push(new User("Ahmed"));
-users.push(new UserAdapter(new GoogleUser("Ahmed","K")));
-users.push(new UserAdapter(new GoogleUser("Jean","Blaguin")));
-users.push(new UserAdapter(new GoogleUser("Sandra","Nicouverture")));
+users.push(new GoogleUser("Ahmed","K"));
+users.push(new GoogleUser("Jean","Blaguin"));
+users.push(new GoogleUser("Sandra","Nicouverture"));
 
 usersRouter.get("/users" , (request, response) => {
     // According GDPR, access to user data will be logged
