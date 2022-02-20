bb77d0b722d5a5dba0f5a39a899d70f683a37cfe
mehdi-elke <m.elketroussi@gmail.com>
2022-02-20 03:31:57 +0100

    add implementatio of prototype

--- src/factories/task-factory.js
+++ src/factories/task-factory.js
@@ -1,15 +1,20 @@
 const PersonalTask = require("../models/personal-task.js")
 const ProfessionalTask = require("../models/professional-task.js")
+const professionalTaskPrototype = new ProfessionalTask(1, "name", false, "company");
 
 class TaskFactory {
     
+    
     create(id, name, completed, company){
         if(company == null){
-            console.log("personal")
             return new PersonalTask(id, name, completed);
         } else {
-            console.log("professional")
-            return new ProfessionalTask(id, name, completed, company);
+            var newProfessionalTask = Object.create(professionalTaskPrototype);
+            newProfessionalTask.id = id;
+            newProfessionalTask.name = name;
+            newProfessionalTask.completed = completed;
+            newProfessionalTask.company = company;
+            return newProfessionalTask;
         }
     }
 }
